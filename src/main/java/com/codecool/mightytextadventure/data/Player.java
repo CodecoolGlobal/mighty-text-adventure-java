fb26e4e3e1cda217bbcc3652679f3cec382336e4
Raphael <raphael.loner@gmail.com>
2023-02-15 14:33:31 +0100

    add some classes

---
+++ src/main/java/com/codecool/mightytextadventure/data/Player.java
@@ -0,0 +1,30 @@
+package com.codecool.mightytextadventure.data;
+
+public class Player {
+
+    public Player(int lives){
+        this.lives = lives;
+    }
+    private String name;
+    private int lives;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getLives(){
+        return lives;
+    }
+    public void looseOneLives(){
+        lives--;
+    }
+
+
+
+
+
+
+}
---
+++ src/main/java/com/codecool/mightytextadventure/data/QuestionAndAnswers.java
@@ -0,0 +1,18 @@
+package com.codecool.mightytextadventure.data;
+
+public class QuestionAndAnswers {
+
+    public QuestionAndAnswers(String question, String answerA, String answerB, String answerC,String correctAnswer){
+      this.question = question;
+        this.answerA = answerA;
+        this.answerB = answerB;
+        this.answerC = answerC;
+        this.correctAnswer = correctAnswer;
+    }
+    public String question;
+    public String answerA;
+    public String answerB;
+    public String answerC;
+    public String correctAnswer;
+
+}
