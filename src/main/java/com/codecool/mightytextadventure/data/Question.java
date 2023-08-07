f082f157d8a3a1c70886bfb2a32f890fa13aba0c
Jovan <jovan.milojkovic@gmx.net>
2023-08-07 20:16:50 +0200

    Code cleaning

--- src/main/java/com/codecool/mightytextadventure/data/Question.java
+++ src/main/java/com/codecool/mightytextadventure/data/Question.java
@@ -1,20 +1,18 @@
 package com.codecool.mightytextadventure.data;
 
 public class Question {
-
-    public Question(String question, String answerA, String answerB, String answerC, String correctAnswer){
-      this.question = question;
-        this.answerA = answerA;
-        this.answerB = answerB;
-        this.answerC = answerC;
-        this.correctAnswer = correctAnswer;
-
-    }
     public String question;
     public String answerA;
     public String answerB;
     public String answerC;
     public String correctAnswer;
 
+    public Question(String question, String answerA, String answerB, String answerC, String correctAnswer) {
+        this.question = question;
+        this.answerA = answerA;
+        this.answerB = answerB;
+        this.answerC = answerC;
+        this.correctAnswer = correctAnswer;
 
+    }
 }
